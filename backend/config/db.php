55ee221d78be6da1e87b0fb536ee8651d7a8e3b0
Takács Dániel Zoltán <takacsd117@gmail.com>
2026-05-01 23:47:52 +0200

    F5MJUS - setup backend WIP

---
+++ backend/config/db.php
@@ -0,0 +1,15 @@
+<?php
+$host = 'localhost';
+$db   = 'vizsga_app';
+$user = 'root';
+$pass = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
+} catch (PDOException $e) {
+    die("Hiba az adatbázis kapcsolatban: " . $e->getMessage());
+}
+?>
